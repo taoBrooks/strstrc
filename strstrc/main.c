99f1f0967e1602c914b352824497552f7111c74a
wangyu <wangyu@wangyudembp.lan>
2014-08-02 16:00:26 +0800

    Initial Commit

---
+++ strstrc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  strstrc
+//
+//  Created by wangyu on 14-8-2.
+//  Copyright (c) 2014年 tz. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
